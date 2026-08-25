36428fef1eb6e592c93267877166e1418b4e8d4a
MarioG0095 <mario.g95@icloud.com>
2026-08-25 22:55:11 +0100

    Add preview book index

---
+++ src/data/book.ts
@@ -0,0 +1,10 @@
+import chapters from './manuscript';
+export type { StoryBlock, PreviewChapter } from './bookTypes';
+const book={
+  title:'Milk, No Sugar',
+  subtitle:'Interactive Preview',
+  author:'Mario Goncalves',
+  tagline:'One boy. One city. A thousand ways to become yourself.',
+  chapters,
+};
+export default book;
